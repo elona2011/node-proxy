014999e2788628ed3669adac776b779d08a9f0c2
elona2011 <cdszbjj@qq.com>
2016-11-09 22:41:04 +0800

    u

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var http = require('http'),
-    httpProxy = require('http-proxy');
+  httpProxy = require('http-proxy');
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000); // See (†)
+httpProxy.createProxyServer({ target: 'http://localhost:9000' }).listen(8000); // See (†)
 
 //
 // Create your target server
 //
 http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  console.log(req)
+
+  http.request({
+    hostname: req.headers.host,
+    port: 80,
+    path: req.url,
+    method: req.method,
+    headers: req.headers
+  }, function (_req, _res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  })
   res.end();
 }).listen(9000);
